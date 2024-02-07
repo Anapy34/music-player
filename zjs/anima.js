22bcf27223ae056b9cea0a9a367ccb0286344569
Anapy <156463751+Anapy34@users.noreply.github.com>
2024-02-07 20:15:41 -0300

    Update anima.js
    03

--- zjs/anima.js
+++ zjs/anima.js
@@ -1,61 +1,4 @@
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const essacx = document.querySelector('.essa');
-//   const btnessa = document.querySelector('.btnessa');
-//   const leftTriangle = document.querySelector('.triangle.left');
-//   const rightTriangle = document.querySelector('.triangle.right');
-
-//   const btnLateral = document.querySelector('.btnLateral');
-//   const bodyLateral = document.querySelector('.abody');
-//   const divLateral = document.querySelector('.divlateral');
-
-//   let isOpen = false;
-//   let isOpen2 = false;
-
-//   function toggleBenVindo() {
-//     var transformValue = getComputedStyle( bodyLateral, divLateral ).transform;
-//     isOpen = !isOpen;
-
-//     if (isOpen) {
-//       // Adiciona a classe para estilizar a div preta     
-//       btnessa.classList.add('brtcor');
-//       // Mova as divs triangulares se a div preta estiver aberta
-//       leftTriangle.style.transform = 'translateX(-100%)';
-//       rightTriangle.style.transform = 'translateX(100%)';
-
-//       // Adiciona um atraso antes de definir z-index como -1
-//       setTimeout(() => {
-//         leftTriangle.style.zIndex = -1;
-//         rightTriangle.style.zIndex = -1;
-//         essacx.style.zIndex = -1;
-//       }, 500);
-
-//     } else {
-//       // Remove a classe para reverter o estilo da div preta   
-//       setTimeout(() => {
-//         btnessa.classList.remove('brtcor');
-//       }, 1000);
-
-//       // Reset as posições das divs triangulares se a div preta estiver fechada
-//       leftTriangle.style.transform = 'translateX(0)';
-//       rightTriangle.style.transform = 'translateX(0)';   
-//       // Defina z-index como -1 para esconder as divs azul e amarela
-//       leftTriangle.style.zIndex = 5;
-//       rightTriangle.style.zIndex = 5;
-//       essacx.style.zIndex = 5;
-//       btnessa.style.zIndex = 6;
-//       bodyLateral.style.transform = 'translateX(0)';
-//       divLateral.style.transform = 'translateX(0)'; 
-
-
-//     }
-
-//     if (transformValue === 'matrix(1, 0, 0, 1, -100, 0)') {
-//       bodyLateral.style.transform = 'translateX(-50%)';
-//     divLateral.style.transform = 'translateX(-50%)'; 
-
 document.addEventListener('DOMContentLoaded', function () {
-  const essacx = document.querySelector('.essa');
   const btnessa = document.querySelector('.btnessa');
   const leftTriangle = document.querySelector('.triangle.left');
   const rightTriangle = document.querySelector('.triangle.right');
@@ -76,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
       setTimeout(() => {
         leftTriangle.style.zIndex = -1;
         rightTriangle.style.zIndex = -1;
-        essacx.style.zIndex = -1;
+        // essacx.style.zIndex = -1;
       }, 500);
 
     } else {
@@ -91,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
       // Defina z-index como -1 para esconder as divs azul e amarela
       leftTriangle.style.zIndex = 5;
       rightTriangle.style.zIndex = 5;
-      essacx.style.zIndex = 5;
+      // essacx.style.zIndex = 5;
       btnessa.style.zIndex = 6;
 
     }
@@ -103,12 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-
-
-// const btnLateral = document.querySelector('.btnLateral');
-// const bodyLateral = document.querySelector('.abody');
-// const divLateral = document.querySelector('.divlateral');
 
 const bodyLateral = document.querySelector('.abody');
 const divLateral = document.querySelector('.divlateral');
@@ -125,6 +62,7 @@ function toggleDiv() {
     divLateral.classList.remove('divlateraladd');
   } else {
     bodyLateral.style.transform = 'translateX(-350px)';
+    bodyLateral.style.transition= 'all 1s'
   //  divLateral.style.transform = 'translateX(0px)';
   divLateral.classList.add('divlateraladd');
   }
